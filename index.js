6367b5b3cc2ac7fcc07d4bd9ee18d574f31eaa73
Omotola Omotayo <126764765+Omomotoly@users.noreply.github.com>
2023-05-22 08:17:06 +0000

    fix index errors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function calculatePolygonAngleSum(n) {
-  if (!Number.isInteger(n) || n < 3) {
-    throw new Error("Number of sides must be an integer greater than or equal to 3.");
-  }
-  const angleSum = (n - 2) * 180;
-  return `The sum of angles in a polygon with ${n} sides is ${angleSum} degrees.`;
+function calculatePolygonAngleSum (n) {
+
+  if ( typeof n !== 'number' ) throw new TypeError()
+  if ( !Number.isInteger(n) || n < 3 ) return 'number of sides must be an integer greater than or equal to 3'
+  //if ( typeof n === 'string' ) n = parseInt(n)
+  return (n-2) * 180;
 }
+
+module.exports = calculatePolygonAngleSum; 
