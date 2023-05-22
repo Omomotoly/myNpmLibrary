deeeb4d180ca97442605f84ab174bf24b2b91334
Omotola Omotayo <126764765+Omomotoly@users.noreply.github.com>
2023-05-22 12:46:22 +0000

    alter index

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# myNpmLibrary
+# Polygon Angle Sum
+
+A simple npm library for calculating the sum of angles in a polygon.
+
+## Installation
+
+To use this library in your project, you need to have Node.js and npm installed. Run the following command to install the package:
+
+```shell
+npm install polygon-angle-sum
+
+
+## Usage
+
+const polygonAngleSum = require('polygon-angle-sum');
+
+// Example usage
+const angles = polygonAngleSum(5);
+console.log(`The sum of angles in a pentagon is ${angles}`);
+
+const polygonAngleSum = require('polygon-angle-calculator');
+
+// Valid polygon
+console.log(polygonAngleSum(4)); // Output: 360
+
+// Invalid number of sides
+console.log(polygonAngleSum(2)); // Output: "Number of sides must be an integer greater than or equal to 3"
+
+// Non-integer number of sides
+console.log(polygonAngleSum(6.5)); // Output: "Number of sides must be an integer greater than or equal to 3"
+
+// Invalid input type
+console.log(polygonAngleSum('6')); // Throws a TypeError
+
+// Invalid input format
+console.log(polygonAngleSum('xyz')); // Throws a TypeError
+
+## Contributing
+ If you want to contribute to the Polygon Angle Sum Calculator, please open an issue or submit a pull request on the GitHub repository.
+
+
--- index.test.js
+++ index.test.js
@@ -1,12 +1,12 @@
-const calculatePolygonAngleSum = require('./index');
+const polygonAngleSum = require('./index');
 
 
-describe ('calculatePolygonAngleSum', () => {
+describe ('polygonAngleSum', () => {
 
 
   test ('should calculate sum of angle for a valid polygon', () => {
 
-    const actualOutput = calculatePolygonAngleSum(7)
+    const actualOutput = polygonAngleSum(7)
     const expectedOutput = 900
 
     expect (actualOutput).toBe(expectedOutput)
@@ -14,7 +14,7 @@ describe ('calculatePolygonAngleSum', () => {
 
   test ('should throw error for invalid number of sides', () => {
 
-    const actualOutput = calculatePolygonAngleSum(2)
+    const actualOutput = polygonAngleSum(2)
     const expectedOutput = 'number of sides must be an integer greater than or equal to 3'
 
     expect (actualOutput).toBe(expectedOutput)
@@ -22,7 +22,7 @@ describe ('calculatePolygonAngleSum', () => {
 
   test ('should throw error for non-integer number of sides', () => {
       
-      const actualOutput = calculatePolygonAngleSum(6.5)
+      const actualOutput = polygonAngleSum(6.5)
       const expectedOutput = 'number of sides must be an integer greater than or equal to 3'
    
       expect (actualOutput).toBe(expectedOutput) 
@@ -30,12 +30,12 @@ describe ('calculatePolygonAngleSum', () => {
 
   test ('should throw error if number of sides is a number string', () => {
 
-    expect (() => calculatePolygonAngleSum('6'))
+    expect (() => polygonAngleSum('6'))
       .toThrow(TypeError)
   })
 
    test('number of polygon side format should be a number', () => {
-    expect (() => calculatePolygonAngleSum('xyz'))
+    expect (() => polygonAngleSum('xyz'))
      .toThrow(TypeError)
   }) 
 })
